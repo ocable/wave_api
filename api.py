2fcfc8ca1b8cb8b63209fc8d05397827dcaf311d
Orion Cable <noiro.elbac@gmail.com>
2025-02-02 20:09:08 -0500

    updated scheduler to run every 90 seconds for testing and global model variable

--- api.py
+++ api.py
@@ -47,14 +47,14 @@ portlandBuoyID = 44007
 # global GFS_model
 # GFS_model = fetch_GFS_model(bull_file
 
-
+GFS_model = []
 
 #Scheduler to fetch GFS model data
 @scheduler.task('interval', id='fetch_GFS_forecast', seconds=90, misfire_grace_time=900)
 def gfsJob():
+    global GFS_model
     date, cycle = UTC_datetime()
     bull_file = requests.get(f'https://nomads.ncep.noaa.gov/pub/data/nccf/com/gfs/prod/gfs.{date}/{cycle}/wave/station/bulls.t{cycle}z/gfswave.{portlandBuoyID}.bull')
-    global GFS_model
     GFS_model = fetch_GFS_model(bull_file)
     print("GFS fetch complete")
     # print(datetime.datetime.now())
@@ -155,9 +155,9 @@ def get_meteorogical_data_route():
 
 
 
-
 @app.route('/GFS')
 def get_GFS_model_route():
+    global GFS_model
     return jsonify(GFS_model)
 
 if __name__ == "__main__":

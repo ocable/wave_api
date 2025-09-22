747160ad62deaeab916d82336ca217030b21ca77
Orion Cable <noiro.elbac@gmail.com>
2025-09-22 17:06:01 -0400

    added energy calculation per forecasted swell comp

--- GFS_model.py
+++ GFS_model.py
@@ -3,6 +3,7 @@ import re
 import matplotlib.pyplot as plt
 import datetime
 from pytz import timezone
+from tools import wave_energy
 
 
 portlandBuoyID = 44007
@@ -13,6 +14,19 @@ def parse_GFS_model(bull_file):
     entries = []
     tz = timezone('US/Eastern')
 
+    def _to_float(val):
+        try:
+            return float(val)
+        except (TypeError, ValueError):
+            return None
+
+    def _calc_energy(height_str, period_str):
+        h = _to_float(height_str)
+        p = _to_float(period_str)
+        if h is None or p is None:
+            return None
+        return wave_energy(p, h)
+
     if bull_file.status_code == 200:
         raw_data = bull_file.text.split('\n')
 
@@ -21,20 +35,24 @@ def parse_GFS_model(bull_file):
             first_line = re.findall(r'\d+\.\d+|\d+', raw_data[i])
 
             if len(first_line) == 4:
-                 
                 day = first_line[0]
                 hour = first_line[1]
                 significant_wave_height = first_line[2]
                 num_swell_components = first_line[3]
 
-                dict = { "timestamp": datetime.datetime.now(tz), "day": day, "hour": hour, "significant_wave_height": significant_wave_height, "num_swell_components": num_swell_components }
+                dict = { 
+                    "timestamp": datetime.datetime.now(tz),
+                    "day": day,
+                    "hour": hour,
+                    "significant_wave_height": significant_wave_height,
+                    "num_swell_components": num_swell_components
+                }
 
                 entries.append(dict)
 
 
 
             elif len(first_line) == 7:
-                 
                 day = first_line[0]
                 hour = first_line[1]
                 significant_wave_height = first_line[2]
@@ -43,12 +61,23 @@ def parse_GFS_model(bull_file):
                 comp1_period = first_line[5]
                 comp1_dir = first_line[6]
 
-                dict = { "timestamp": datetime.datetime.now(tz), "day": day, "hour": hour, "significant_wave_height": significant_wave_height, "num_swell_components": num_swell_components, "comp1_height": comp1_height, "comp1_period": comp1_period, "comp1_dir": comp1_dir }
+                comp1_energy = _calc_energy(comp1_height, comp1_period)
+
+                dict = { 
+                    "timestamp": datetime.datetime.now(tz),
+                    "day": day,
+                    "hour": hour,
+                    "significant_wave_height": significant_wave_height,
+                    "num_swell_components": num_swell_components,
+                    "comp1_height": comp1_height,
+                    "comp1_period": comp1_period,
+                    "comp1_dir": comp1_dir,
+                    "comp1_energy": comp1_energy
+                }
 
                 entries.append(dict)
             
             elif len(first_line) == 10:
-                 
                 day = first_line[0]
                 hour = first_line[1]
                 significant_wave_height = first_line[2]
@@ -56,11 +85,29 @@ def parse_GFS_model(bull_file):
                 comp1_height = first_line[4]
                 comp1_period = first_line[5]
                 comp1_dir = first_line[6]
-                comp2_height = [7]
-                comp2_period = [8]
-                comp2_dir = [9]
+                comp2_height = first_line[7]
+                comp2_period = first_line[8]
+                comp2_dir = first_line[9]
 
-                dict = { "timestamp": datetime.datetime.now(tz), "day": day, "hour": hour, "significant_wave_height": significant_wave_height, "num_swell_components": num_swell_components, "comp1_height": comp1_height, "comp1_period": comp1_period, "comp1_dir": comp1_dir, "comp2_height": comp2_height, "comp2_period": comp2_period, "comp2_dir": comp2_dir }
+                # Energy calculations
+                comp1_energy = _calc_energy(comp1_height, comp1_period)
+                comp2_energy = _calc_energy(comp2_height, comp2_period)
+
+                dict = { 
+                    "timestamp": datetime.datetime.now(tz),
+                    "day": day,
+                    "hour": hour,
+                    "significant_wave_height": significant_wave_height,
+                    "num_swell_components": num_swell_components,
+                    "comp1_height": comp1_height,
+                    "comp1_period": comp1_period,
+                    "comp1_dir": comp1_dir,
+                    "comp1_energy": comp1_energy,
+                    "comp2_height": comp2_height,
+                    "comp2_period": comp2_period,
+                    "comp2_dir": comp2_dir,
+                    "comp2_energy": comp2_energy
+                }
 
                 entries.append(dict)
 
@@ -79,7 +126,29 @@ def parse_GFS_model(bull_file):
                 comp3_period = first_line[11]
                 comp3_dir = first_line[12]
 
-                dict = { "timestamp": datetime.datetime.now(tz), "day": day, "hour": hour, "significant_wave_height": significant_wave_height, "num_swell_components": num_swell_components, "comp1_height": comp1_height, "comp1_period": comp1_period, "comp1_dir": comp1_dir, "comp2_height": comp2_height, "comp2_period": comp2_period, "comp2_dir": comp2_dir, "comp3_height": comp3_height, "comp3_period": comp3_period, "comp3_dir": comp3_dir }
+                comp1_energy = _calc_energy(comp1_height, comp1_period)
+                comp2_energy = _calc_energy(comp2_height, comp2_period)
+                comp3_energy = _calc_energy(comp3_height, comp3_period)
+
+                dict = { 
+                    "timestamp": datetime.datetime.now(tz),
+                    "day": day,
+                    "hour": hour,
+                    "significant_wave_height": significant_wave_height,
+                    "num_swell_components": num_swell_components,
+                    "comp1_height": comp1_height,
+                    "comp1_period": comp1_period,
+                    "comp1_dir": comp1_dir,
+                    "comp1_energy": comp1_energy,
+                    "comp2_height": comp2_height,
+                    "comp2_period": comp2_period,
+                    "comp2_dir": comp2_dir,
+                    "comp2_energy": comp2_energy,
+                    "comp3_height": comp3_height,
+                    "comp3_period": comp3_period,
+                    "comp3_dir": comp3_dir,
+                    "comp3_energy": comp3_energy
+                }
 
                 entries.append(dict)
                  
